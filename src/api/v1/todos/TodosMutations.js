95a35e8fb1bef29f2e8c3e06840a964abfc6a07c
banu-i <152266442+banu-i@users.noreply.github.com>
2024-02-21 11:06:00 +0100

    20240220 - Kursaufgabe : 20240221 - Unterricht Ergänzung zu REACT APP 20240220 und Express App 20240216 J

--- src/api/v1/todos/TodosMutations.js
+++ src/api/v1/todos/TodosMutations.js
@@ -9,5 +9,27 @@ async function createTodo(newTodo) {
 
   return todos;
 }
+// PUT /mark
+async function markTodo(id, isDone) {
+  const result = await api.put("/todos/mark", { id, isDone });
+  const todomark = result.data;
 
+  return todomark;
+}
+// PUT /update
+async function fetchTodosUpdate(todosUpdate) {
+  const result = await api.put("/todos/update", { params: { todosUpdate } });
+
+  const todoupdate = result.data.todosupdate;
+
+  return todoupdate;
+}
+// DELETE /delete
+async function fetchTodosDelete(todosDelete) {
+  const result = await api.delete("/todos/delete", { params: { todosDelete } });
+
+  const tododelete = result.data.todosdelete;
+
+  return tododelete;
+}
 export default { createTodo };
--- src/api/v1/todos/TodosQueries.js
+++ src/api/v1/todos/TodosQueries.js
@@ -16,37 +16,14 @@ async function fetchTodoById(todoId) {
   return todo;
 }
 // GET /byuserid
-async function fetchTodosByUserId(todosByUserId) {
+async function fetchTodosByUserId(userId) {
   const result = await api.get("/todos/byuserid", {
-    params: { todosByUserId },
+    params: { userId },
   });
 
-  const todobyuserid = result.data.todosbyuserid;
+  const todobyuserid = result.data;
 
   return todobyuserid;
 }
-// PUT /mark
-async function fetchTodosMark(todosMark) {
-  const result = await api.put("/todos/mark", { params: { todoMark } });
 
-  const todomark = result.data.todosmark;
-
-  return todomark;
-}
-// PUT /update
-async function fetchTodosUpdate(todoId) {
-  const result = await api.put("/todos/update", { params: { todosUpdate } });
-
-  const todoupdate = result.data.todosupdate;
-
-  return todoupdate;
-}
-// DELETE /delete
-async function fetchTodosDelete(todosDelete) {
-  const result = await api.delete("/todos/delete", { params: { todosDelete } });
-
-  const tododelete = result.data.todosdelete;
-
-  return tododelete;
-}
 export default { fetchAllTodos, fetchTodoById };
